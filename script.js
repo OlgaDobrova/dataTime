39528ce02720e20ac564436f20c23189b6b44deb
Olga Dobrova <dobrova_o_g@mail.ru>
2023-10-01 13:55:07 +0300

    таймер: тек.вр

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 "use strict";
-//Это Урок №9 усложненное задание
+//Это Урок №19 обязательное задание
 
-const currentDateFormat = setInterval(function () {
+const welcome = document.querySelector(".welcome");
+const dayWeek = document.querySelector(".current-day-week");
+const time = document.querySelector(".current-time");
+const residue = document.querySelector(".number-days-until-new-year");
+
+//вычисление времени до дедлайна
+const getTimeRemaining = (deadline) => {
   const dayWeek = [
     "Воскресенье",
     "Понедельник",
@@ -11,24 +17,6 @@ const currentDateFormat = setInterval(function () {
     "Пятница",
     "Суббота",
   ];
-  const monthTitle = [
-    "января",
-    "февраля",
-    "марта",
-    "апреля",
-    "мая",
-    "июня",
-    "июля",
-    "августа",
-    "сентября",
-    "октября",
-    "ноября",
-    "декабря",
-  ];
-
-  let toDay = new Date();
-  let fullCurrentDate = document.querySelector(".full-current-date");
-  let shortCurrentDate = document.querySelector(".short-current-date");
 
   const hourWord = function (value, words) {
     value = Math.abs(value) % 100;
@@ -39,40 +27,73 @@ const currentDateFormat = setInterval(function () {
     return words[2];
   };
 
-  fullCurrentDate.innerHTML =
-    "Сегодня " +
-    dayWeek[toDay.getDay()] +
-    ", " +
-    toDay.getDate() +
-    " " +
-    monthTitle[toDay.getMonth()] +
-    " " +
-    toDay.getFullYear() +
-    ", " +
-    toDay.getHours() +
-    " " +
-    hourWord(toDay.getHours(), ["час", "часа", "часов"]) +
-    " " +
-    toDay.getMinutes() +
-    " " +
-    hourWord(toDay.getMinutes(), ["минута", "минут", "минут"]) +
-    " " +
-    toDay.getSeconds() +
-    " " +
-    hourWord(toDay.getSeconds(), ["секунда", "секунды", "секунд"]);
+  let toDay = new Date(); //текущая дата
 
-  shortCurrentDate.innerHTML =
-    toDay.getDate().toString().padStart(2, "0") +
-    "." +
-    toDay.getMonth().toString().padStart(2, "0") +
-    "." +
-    toDay.getFullYear() +
-    " - " +
-    toDay.getHours().toString().padStart(2, "0") +
-    ":" +
-    toDay.getMinutes().toString().padStart(2, "0") +
-    ":" +
-    toDay.getSeconds().toString().padStart(2, "0");
-}, 1000);
+  let currentDayWeek = dayWeek[toDay.getDay()];
+  let currentHours = toDay.getHours();
+  let currentMinutes = toDay.getMinutes();
+  let currentSeconds = toDay.getSeconds();
 
-currentDateFormat();
+  let timesOfDay = ""; //время суток
+
+  let dateStop = new Date(deadline).getTime(); //дата дедлайна в миллисекундах
+  let dateNow = new Date().getTime(); //текущая дата  в миллисекундах
+  //разница - кол-во миллисекунд до дедлайна
+  //1сек = 1000 миллисекунд
+  let timeRemaining = (dateStop - dateNow) / 1000; //кол-во секунд
+  let days = Math.floor(timeRemaining / 60 / 60 / 24);
+  let daysWord = hourWord(Math.floor(timeRemaining / 60 / 60 / 24), [
+    "день",
+    "дня",
+    "дней",
+  ]);
+
+  switch (true) {
+    case currentHours <= 5 || currentHours >= 23: {
+      timesOfDay = "Доброй ночи";
+      break;
+    }
+    case currentHours >= 6 && currentHours <= 10: {
+      timesOfDay = "Доброе утро";
+      break;
+    }
+    case currentHours >= 11 && currentHours <= 15: {
+      timesOfDay = "Добрый день";
+      break;
+    }
+    default: {
+      timesOfDay = "Добрый вечер";
+      break;
+    }
+  }
+
+  return {
+    timeRemaining,
+    days,
+    daysWord,
+    timesOfDay,
+    currentDayWeek,
+    currentHours,
+    currentMinutes,
+    currentSeconds,
+  };
+};
+
+const updateClock = (deadline) => {
+  let getTime = getTimeRemaining(deadline);
+
+  if (getTime.timeRemaining > 0) {
+    welcome.textContent = getTime.timesOfDay;
+    dayWeek.textContent = `Сегодня: ${getTime.currentDayWeek}`;
+    time.textContent = `Текущее время: ${getTime.currentHours
+      .toString()
+      .padStart(2, "0")}:${getTime.currentMinutes
+      .toString()
+      .padStart(2, "0")}:${getTime.currentSeconds
+      .toString()
+      .padStart(2, "0")} PM`;
+    residue.textContent = `До нового года осталось: ${getTime.days} ${getTime.daysWord}`;
+  }
+};
+
+setInterval(updateClock, 1000, "01 january 2024");
